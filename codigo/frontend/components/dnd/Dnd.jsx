18b5acb354b807ff38b508ba707e5d9b55887001
jesusparfer27 <jesusparfer@alumnos.cei.es>
2024-03-03 23:39:55 +0100

    avance xd

---
+++ codigo/frontend/components/dnd/Dnd.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import './Dnd.css'
+
+const Dnd = () => {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const controller = new AbortController();
+            const options = {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                signal: controller.signal
+            };
+
+            try {
+                const response = await fetch('https://www.dnd5eapi.co/api/classes/', options);
+                const data = await response.json();
+                setCharacters(data.results);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                controller.abort();
+            }
+        };
+
+        fetchData();
+
+        return () => {
+            // Cleanup function to abort the fetch if the component is unmounted
+           
+        };
+    }, []);
+
+    return (
+        <>
+            <h1>Characters Fetch</h1>
+            <ul>
+                {characters.map((character) => (
+                    <li key={character.index}>{character.name}
+                    <a href={character.url}></a></li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+export default Dnd;
---
+++ codigo/frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import Dnd from '../components/dnd/Dnd';
+import FakeStore from '../components/fake-store/FakeStore';
+import PokeApi from '../components/poke-api/PokeApi';
+import './App.css';
+
+function App() {
+  const [selectedLink, setSelectedLink] = useState(null);
+  const [actualPage, setActualPage] = useState("Dnd");
+
+  const handleLinkClick = (page) => {
+    setActualPage(page);
+    setSelectedLink(null);
+  };
+
+  return (
+    <>
+      <nav>
+        <span
+          className={`NavLink ${actualPage === "Dnd" ? "active" : ""}`}
+          onClick={() => handleLinkClick("Dnd")}
+        >
+          Dnd
+        </span>
+        <span
+          className={`NavLink ${actualPage === "FakeStore" ? "active" : ""}`}
+          onClick={() => handleLinkClick("FakeStore")}
+        >
+          FakeStore
+        </span>
+        <span
+          className={`NavLink ${actualPage === "PokeApi" ? "active" : ""}`}
+          onClick={() => handleLinkClick("PokeApi")}
+        >
+          PokeApi
+        </span>
+      </nav>
+      <div>
+        {actualPage === "Dnd" && <Dnd />}
+        {actualPage === "FakeStore" && <FakeStore />}
+        {actualPage === "PokeApi" && <PokeApi />}
+      </div>
+    </>
+  );
+}
+
+export default App;
